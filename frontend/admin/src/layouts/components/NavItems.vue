74cc334c02c600b22e19c51f339f1b59fcb9c6ac
furkanturan8 <turanfurkan830@gmail.com>
2025-04-10 22:02:07 +0300

    frontend template base directories added (this template uses from ThemeSelection:https://themeselection.com/item/category/admin-templates/)

---
+++ frontend/admin/src/layouts/components/NavItems.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+import VerticalNavSectionTitle from '@layouts/components/VerticalNavSectionTitle.vue'
+</script>
+
+<template>
+  <!-- 👉 Home -->
+  <VerticalNavLink
+    :item="{
+      title: 'Anasayfa',
+      icon: 'bx-home-smile',
+      to: '/dashboard',
+    }"
+  />
+</template>
---
+++ frontend/admin/src/plugins/iconify/index.ts
@@ -0,0 +1,4 @@
+
+export default function () {
+  // This plugin just requires icons import
+}
